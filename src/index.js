accec27b523a8e38077e316ae159ff3e9db234b9
wrublik93 <wrublik.93@mail.ru>
2020-03-02 20:27:14 +0300

    feat: implement function decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    /*check length of encoded letters*/
+    if(expr.length % 10 !== 0) {
+        do {
+            expr = expr + '0';
+        } while (expr.length % 10 === 0)
+    };
+    let arrayLetters = [];
+
+    /*split a string of 10 characters*/
+    for(let i = 0; i < expr.length; i += 10){
+        arrayLetters.push(expr.slice(i, i + 10));
+    };
+    let decodeString = '';
+    arrayLetters.forEach(function(item){
+
+        /*check space in string*/
+        if(item == '**********') {
+            decodeString = decodeString + ' ';
+        } else {
+
+            /*split a string of letter of 2 characters*/
+            let arrayIndividualLetter = [];
+            for(let i = 0; i < item.length; i += 2){
+                arrayIndividualLetter.push(item.slice(i, i + 2));
+            };
+
+            /*search dots and dashes*/
+            let codeString = '';
+            arrayIndividualLetter.forEach(function(letter){
+            if(letter == '10') {
+                codeString = codeString + '.';
+            };
+            if(letter == '11') {
+                codeString = codeString + '-';
+            };
+            });
+            decodeString = decodeString + MORSE_TABLE[codeString];
+        }
+    })
+    return decodeString;
 }
 
 module.exports = {
